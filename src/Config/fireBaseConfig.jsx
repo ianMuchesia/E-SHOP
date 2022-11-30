cf1e20a71a08a1c28dc61e4cc773d68f63179cdc
ianMuchesia <ianmuchesia@yahoo.com>
2022-11-30 01:03:04 -0800

    CHORE import through .env

---
+++ src/Config/fireBaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDv1OhRb-dWQY6quADCjb6kcgJzHns2WSQ",
+  authDomain: "e-shop-7b24d.firebaseapp.com",
+  projectId: "e-shop-7b24d",
+  storageBucket: "e-shop-7b24d.appspot.com",
+  messagingSenderId: "363760647948",
+  appId: "1:363760647948:web:d2619995d13729ae151329"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
