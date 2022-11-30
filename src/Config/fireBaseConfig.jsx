aa2752f864dbf8f3b66a15496244b1a90b6a590d
ianMuchesia <ianmuchesia@yahoo.com>
2022-11-30 01:20:47 -0800

    saved the env file

--- src/Config/fireBaseConfig.jsx
+++ src/Config/fireBaseConfig.jsx
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDv1OhRb-dWQY6quADCjb6kcgJzHns2WSQ",
-  authDomain: "e-shop-7b24d.firebaseapp.com",
-  projectId: "e-shop-7b24d",
-  storageBucket: "e-shop-7b24d.appspot.com",
-  messagingSenderId: "363760647948",
-  appId: "1:363760647948:web:d2619995d13729ae151329"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID ,
+  storageBucket:import.meta.env.VITE_STORAGE_BUCKET ,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
