bf760c4b32d731c8f3a19efb9933910d8d1aab42
ianMuchesia <ianmuchesia@yahoo.com>
2022-12-03 04:51:54 -0800

    CHORE add Cart

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -6,21 +6,8 @@ import { fetchProducts } from "../../store/productSlice";
 
 import ShopProduct from "./ShopProduct";
 
-const Shop = () => {
-  const dispatch = useDispatch();
-
-  const product = useSelector((state) => state.product);
-
-  const loading = useSelector((state) => state.loading);
-
-  //fetch products from firestore
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, []);
-
-  //loading spinner
-  if (product.loading) return <Spinner message="loading..." />;
-  //console.log(product.products)
+const Shop = ({product}) => {
+  
 
   return (
     <div className="p-5 font-sans sm:grid sm:grid-cols-3 gap-4 lg:grid-cols-4 ">
@@ -29,15 +16,15 @@ const Shop = () => {
         <h2 className="text-2xl text-center sm:m-2 sm:font-bold">Products</h2>
         <div className="grid place-items-center gap-4  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           <hr className="w-full border-2 sm:hidden" />
-          {!loading &&
-            product.products.map((product) => {
+          {!product.loading &&
+            product.products.map((item) => {
               return (
                 <ShopProduct
-                  key={product.id}
-                  id={product.id}
-                  image={product.imageUrl}
-                  name={product.name}
-                  price={product.price}
+                  key={item.id}
+                  id={item.id}
+                  image={item.imageUrl}
+                  name={item.name}
+                  price={item.price}
                 />
               );
             })}
